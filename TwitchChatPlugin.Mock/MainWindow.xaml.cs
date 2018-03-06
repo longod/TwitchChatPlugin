05626b6d2bdc8aca7eaada3097e8bfd2e5651f0b
longod <forferelden@gmail.com>
2018-03-06 09:27:18 +0900

    implement chat with latency and spam suppression exclude yukarinette logger for test

--- TwitchChatPlugin.Mock/MainWindow.xaml.cs
+++ TwitchChatPlugin.Mock/MainWindow.xaml.cs
@@ -9,12 +9,35 @@ namespace TwitchChatPlugin.Mock {
             InitializeComponent();
         }
 
-        private void Button_Click( object sender, RoutedEventArgs e ) {
-            Settings settings = Settings.Load( Settings.Filename );
+        private void Settings_Click( object sender, RoutedEventArgs e ) {
+            settings = Settings.Load( Settings.Filename );
             var s = new SettingsWindow();
             if ( s.Show( Application.Current.MainWindow, settings ) ) {
                 settings = s.Accept();
                 Settings.Save( Settings.Filename,  settings );
+            }
+        }
+
+        private void Connect_Click( object sender, RoutedEventArgs e ) {
+            if ( settings == null ) {
+                settings = Settings.Load( Settings.Filename );
+            }
+            client = new ChatClient( settings.username, Cryptography.Decrypt( settings.oauth ), settings.Latency, settings.ProxyIP, settings.Port, true );
+            client?.Connect();
+        }
+
+        private void Disconnect_Click( object sender, RoutedEventArgs e ) {
+            client?.Disconnect();
+            client = null; // dispose?
+        }
+
+        ChatClient client = null;
+        Settings settings = null;
+
+        private void Send_Click( object sender, RoutedEventArgs e ) {
+            string text = textboxSend.Text;
+            if ( text != null ) {
+                client?.EnqueueMessage( text );
             }
         }
     }
